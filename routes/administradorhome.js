4d30102565cf930ba7a223a6457e8f023c27e162
LuisFernandoHedzHedz005 <luisfdohernandez005@gmail.com>
2025-04-28 23:13:21 -0600

    arreglando rutas

--- routes/administradorhome.js
+++ routes/administradorhome.js
@@ -15,6 +15,7 @@ const express = require('express');
 const router = express.Router();
 const administradorhomeController = require('../controllers/administradorhomeController');
 const editarProfesor = require('../controllers/editarProfesorController');
+const anadirVisitante = require('../controllers/anadirVisitanteController');
 
 // Ruta para el dashboard principal
 router.get('/', administradorhomeController.mostrarTabla);;
@@ -30,5 +31,11 @@ router.delete('/eliminar/:id', administradorhomeController.eliminarProfesor);
 
 router.get('/editarProfesor/:id', editarProfesor.mostrarFormulario);
 router.post('/editarProfesor/:id', editarProfesor.actualizarProfesor);
+
+
+router.delete('/eliminarVisitante/:id', administradorhomeController.eliminarVisitante);
+router.get('/anadirVisitante', anadirVisitante.mostrarFormulario);
+router.post('/anadirVisitante', anadirVisitante.guardarVisitante);
+
 
 module.exports = router;
